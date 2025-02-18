cdfb7983603b311fcbb82c7aa981e61c70858c47
Shavidu Keshan <shavidu4321@gmail.com>
2025-02-18 14:50:56 +0530

    intial commit

---
+++ atdigital/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-gray-800">
+        Hello, my new app!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
